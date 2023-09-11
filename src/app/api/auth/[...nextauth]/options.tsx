52c62180c3a7a7dfbb99d29a6dfe172f2ab9acdc
Abebe Bediye <abebe.bediye@gmail.com>
2023-09-11 06:01:51 +0200

    added next auth and session provider

---
+++ src/app/api/auth/[...nextauth]/options.tsx
@@ -0,0 +1,24 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+const prisma: PrismaClient = new PrismaClient()
+
+export const options: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        }),
+        GithubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        }),
+    ],
+    session: {
+        strategy: 'jwt'
+    }
+}
